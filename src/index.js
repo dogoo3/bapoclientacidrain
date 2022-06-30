688710ce1abb72885119ad61b1ccd2c3b59eca85
DO <as.sd.show@gmail.com>
2022-06-30 10:50:11 +0900

    change localhost

--- src/index.js
+++ src/index.js
@@ -7,9 +7,9 @@ import AcidLogic from "./service/logic";
 import SocketIO from "./service/socket";
 
 // const baseURL = process.env.REACT_APP_BASE_URL;
-const httpClient = new HttpClient("http://localhost:7000");
+const httpClient = new HttpClient("https://acidrainmogi.herokuapp.com/:7000");
 const acidlogic = new AcidLogic(httpClient);
-const socketIO = new SocketIO("http://localhost:7000");
+const socketIO = new SocketIO("https://acidrainmogi.herokuapp.com/:7000");
 
 socketIO.CheckError();
 
